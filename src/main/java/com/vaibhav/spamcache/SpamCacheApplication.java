6597bc2b9e330684e962567f51d1149ce1505185
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-02-07 13:24:25 +0530

    initial setup

---
+++ src/main/java/com/vaibhav/spamcache/SpamCacheApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhav.spamcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpamCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaibhav/spamcache/SpamCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhav.spamcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
